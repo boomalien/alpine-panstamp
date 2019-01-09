58ad0318104e458cb708995c80ab06517b7c2f07
Oliver <boomalien@clickboom.de>
2019-01-09 18:54:48 +0100

    added user and usergroup id's

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ Write-Host Starting build
 if ($isWindows) {
   docker build --pull -t alpine-panstamp -f Dockerfile.windows .
 } else {
-  docker build -t alpine-panstamp --build-arg "arch=$env:ARCH" .
+  docker build -t alpine-panstamp --build-arg "arch=$env:ARCH" --build-arg USER_ID=999 --build-arg GROUP_ID=998 .
 }
 
 docker images
